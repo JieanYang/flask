ac58c8a5b36d47d8d52428bcc8478059653f89a1
JieanYang <jiean.yang.24@gmail.com>
2019-01-18 17:16:12 +0100

    Debug

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,15 +10,6 @@ from flaskr.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-def login_required(view):
-	@functools.wraps(view)
-	def wrapped_view(**kwargs):
-		if g.user is None:
-			return redirect(user_for('auth.login'))
-
-		return view(**kwargs)
-
-	return wrapped_view
 
 @bp.before_app_request
 def load_logged_in_user():
@@ -28,9 +19,19 @@ def load_logged_in_user():
 	if user_id is None:
 		g.user = None
 	else:
-		g.user = get_db.execute(
+		g.user = get_db().execute(
 			'SELECT * FROM user WHERE id = ?', (user_id,)
 		).fetchone()
+
+def login_required(view):
+	@functools.wraps(view)
+	def wrapped_view(**kwargs):
+		if g.user is None:
+			return redirect(user_for('auth.login'))
+
+		return view(**kwargs)
+
+	return wrapped_view
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():

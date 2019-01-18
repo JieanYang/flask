3cbde5d559e82461d913242b62790d282f238225
JieanYang <jiean.yang.24@gmail.com>
2019-01-18 15:43:45 +0100

    Debug

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -82,7 +82,7 @@ def login():
 		if error is None:
 			session.clear()
 			session['user_id'] = user['id']
-			return redirect(user_for('index'))
+			return redirect(url_for('index'))
 
 		flash(error)
 
